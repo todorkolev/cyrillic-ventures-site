e232cb5db7ec44e445cb6edbb31993e07309e622
Todor Kolev <tkolev@obecto.com>
2018-10-22 23:44:35 +0300

    Code import

---
+++ src/assets/js/main.js
@@ -0,0 +1,41 @@
+;(function(window, document, $) {
+	var $win = $(window);
+	var $doc = $(document);
+	var winH = $win.height();
+
+	// Animate Function
+	function animate(winST) {
+		$('.animations').each(function(){
+			if (winST + winH * 0.9 > $(this).offset().top) {
+				var $this = $(this);
+				var animClass = $this.data('animation');
+
+				if ( !$this.hasClass('animated') ) {
+					$this.addClass( 'animated' );
+					$this.addClass( animClass );
+				}
+			}
+		});
+	};
+
+	$('.btn--open-members').on('click', function(event) {
+		$('.section--team').addClass('visible');
+		
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $("#team-section").offset().top
+		}, 1000);
+
+		event.preventDefault();
+	});
+
+	$('.btn--close-members').on('click', function(event) {
+		$('.section--team').removeClass('visible');		
+		event.preventDefault();
+	});
+
+	//Window load event
+	$win.on('load scroll', function () {
+		var winST = $win.scrollTop();
+		animate(winST);
+	});
+})(window, document, window.jQuery);
